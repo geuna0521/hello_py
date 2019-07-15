ddd4e9e244a8d5199663c4ee7d3b0469b77853de
Jieun <geuna0521@gmail.com>
2019-07-16 03:46:42 +0900

    test_case(cycle) 횟수만큼 입력받아 합 출력하기

---
+++ prac/sigmaN.py
@@ -0,0 +1,10 @@
+# boj https://www.acmicpc.net/problem/8393
+
+num = int(input())
+
+sum = 0
+
+for number in range(1, num + 1):
+    sum = sum + number
+
+print(sum)
---
+++ prac/testcase_sum0.py
@@ -0,0 +1,18 @@
+# boj https://www.acmicpc.net/problem/10950
+# test_case = input()
+# test_case = int(test_case)
+
+# for num in range(0, test_case):
+#    a, b = input().split()
+#    sum = int(a) + int(b)
+#    print(sum)
+
+
+T=int(input())
+for i in range(T):
+    string=input()
+    A,B=string.split()
+    A=int(A)
+    B=int(B)
+    print(A+B)
+
